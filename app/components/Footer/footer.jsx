2176bffadae0acc977a91585a13a0b0408f83b40
Suneel-Developer <suneeldeveloper11@gmail.com>
2026-02-20 10:48:41 +0500

    first commit

---
+++ app/components/Footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='py-18 px-9 bg-[#0f0e0d] max-w-432 w-full mx-auto'>
+            <section className='border-b border-[#33312c] flex items-center justify-between pb-18'>
+                <h3 className='text-[32px] text-white text-balance mb-1'>
+                    Unlock Professional Class AI for Your Firm
+                </h3>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Footer from "./components/Footer/footer";
+
+export default function Home() {
+  return (
+    <main className="harvey text-7xl font-normal -tracking-[0.9px] text-balance">
+      Practice Made Perfect
+      <Footer />
+    </main>
+  );
+}
